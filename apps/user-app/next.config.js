a681ebe8cfd9ea08240624285127c6ecde2790b5
chisty <mchisty31@gmail.com>
2024-04-07 00:19:24 +0600

    next auth added

--- apps/user-app/next.config.js
+++ apps/user-app/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   transpilePackages: ["@repo/ui"],
+  images: {
+    domains: ['assets.aceternity.com'],
+  },
 };
+
+// next.config.js
+
+
