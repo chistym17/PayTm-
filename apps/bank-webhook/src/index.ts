05c8ceaf68622a2688284fae1d36d8717b1b7995
chisty <mchisty31@gmail.com>
2024-04-23 12:39:51 +0600

    webhook server logic added

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,13 +1,52 @@
+import { PrismaClient } from "@prisma/client";
 import express from "express";
 
 const app = express();
-
-app.post("/hdfcWebhook", (req, res) => {
-    //TODO: Add zod validation here?
+const prisma = new PrismaClient()
+app.post("/hdfcWebhook", async (req, res) => {
     const paymentInformation = {
         token: req.body.token,
         userId: req.body.user_identifier,
         amount: req.body.amount
     };
-    // Update balance in db, add txn
+
+    try {
+
+        await prisma.balance.update({
+            where: {
+                userId: Number(paymentInformation.userId)
+
+            },
+            data: {
+                amount:
+                {
+                    increment: Number(paymentInformation.amount)
+                },
+            }
+
+        }),
+
+
+            await prisma.onRampTransaction.update({
+                where: {
+                    token: (paymentInformation.token)
+                },
+                data: {
+                    status: 'Success'
+                },
+
+            })
+
+        res.json({
+            message: "Captured"
+        })
+
+
+    } catch (e) {
+        console.error(e);
+        res.status(411).json({
+            message: "Error while processing webhook"
+        })
+    }
+
 })
