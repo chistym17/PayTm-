a6e0ad05d697e6cd800aac486bd08561be691d88
chisty <mchisty31@gmail.com>
2024-07-01 15:57:01 +0600

    checking cd

--- apps/user-app/components/userbar.tsx
+++ apps/user-app/components/userbar.tsx
@@ -43,7 +43,7 @@ const UserBar = () => {
         onClick={handleDashboardClick}
         className="bg-blue-500 hover:bg-blue-600 py-2 px-4 w-[200px] text-center rounded-md shadow-md transition duration-300"
       >
-        Dashboard
+        Dash
       </button>
     )}
   </div>
